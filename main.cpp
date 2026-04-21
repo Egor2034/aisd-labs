b31d1421a47aba54c50c323675fff3013d13ed9d
Egor2034 <egor_2061@mail.ru>
2026-04-21 19:09:38 +0400

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
 #include <windows.h>
-#include "tree.hpp"
-#include "tests.cpp"
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
     
-    tests();
     
-    std::vector<int> vect { 2, 2, 3, 4, 5, 5 };
-    std::vector<int> uniq = uniq_elements(vect);
-
-    for (int i = 0; i < uniq.size(); i++) {
-        std::cout << uniq[i] << " ";
-    }
 
     std::cout << "\n";
 
